7651012f354aa09fd3b7d0d470846c7b3d05f7fe
blackneko <153214823+blackneko39@users.noreply.github.com>
2024-04-15 14:41:09 +0900

    refactoring some codes

--- src/app/_components/footer.jsx
+++ src/app/_components/footer.jsx
@@ -1,9 +1,13 @@
+import Link from 'next/link';
 import './footer.scss';
 
 export default function Footer() {
     return (
       <footer className="ft">
-        <p id='copyright'>© 2024 Kinoshita, Ryosuke</p>
+        <ul className='txt-list'>
+          <li><p className='txt'>Source code is <Link className='txt-link' href='https://github.com/blackneko39/HomePage'>here</Link> on GitHub</p></li>
+          <li><p className='txt'>© 2024 Kinoshita, Ryosuke</p></li>
+        </ul>
       </footer>
     );
 }
